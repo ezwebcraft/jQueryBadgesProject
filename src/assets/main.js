56fc5603ba5b0df434310c71f6141c79a58c62dd
Elvin Ramirez <real8686@gmail.com>
2017-07-08 18:01:59 -0400

    feat: add json and new func

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,17 @@
 $(function() {
-
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/Ramirez1900.json',
+    dataType: 'jsonp',
+    success: function(data) {
+    populateWithCourses(data.courses.completed)
+  })
 
 });
+
+
+function CSCourses(courses) {
+
+   var $badges = $('#badges');
+
+ });

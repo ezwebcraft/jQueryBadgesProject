a9ece050cbde57f5bd11137ad57d4008e2036436
Elvin Ramirez <real8686@gmail.com>
2017-07-08 18:14:16 -0400

    feat: completed the array for codeschool

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,17 +1,40 @@
 $(function() {
-  // your code will go here
+
   $.ajax({
     url: 'https://www.codeschool.com/users/Ramirez1900.json',
     dataType: 'jsonp',
     success: function(data) {
-    populateWithCourses(data.courses.completed)
-  })
+      CodeSchool(data.courses.completed)
+    }
+  });
+
+  function CodeSchool(courses) {
+
+    var $badges = $('#badges');
+
+    courses.forEach(function(course) {
+
+      $div = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($div);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($div);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo($div);
+
+    });
+
+  }
 
 });
-
-
-function CSCourses(courses) {
-
-   var $badges = $('#badges');
-
- });
